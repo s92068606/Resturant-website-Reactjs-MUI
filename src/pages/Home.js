2140b55d9483efaeb6d1d662320573e66dc5182d
S92068606 <s92068606@ousl.lk>
2024-05-05 00:05:53 +0530

    updates the frnt end works

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,24 @@
 import React from "react";
 import Layout from "./../components/Layout/Layout";
 import { Link } from "react-router-dom";
-import Banner from "../images/banner.jpeg";
 import "../styles/HomeStyles.css";
 
 const Home = () => {
   return (
     <Layout>
-      <div className="home" style={{ backgroundImage: `url(${Banner})` }}>
+      <div className="home">
         <div className="headerContainer">
-          <h1>Food Website</h1>
-          <p>Best Food In India</p>
           <Link to="/menu">
             <button>GET FREE CONSULTATION</button>
           </Link>
+          <div className="rectangle-box">
+          {/* Content inside the rectangle box */}
+          <h2>What is Webflow and why is it the best website builder?</h2>
+          <br></br><br></br>
+          <h2>What is Webflow and why is it the best website builder?</h2>
+          <p>Vitae congue eu consequat ac felis placerat vestibulum lectus mauris ultrices. Cursus sit amet dictum sit amet justo donec enim diam porttitor lacus luctus accumsan tortor posuere.</p>
+          
+        </div>
         </div>
       </div>
     </Layout>
